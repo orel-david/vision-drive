98e10501b2c7ed778a75fe4fd38db479c6681c6d
oreld <orel.david1502@gmail.com>
2019-09-29 11:07:23 +0300

    Create a command that would do vision drive
    with proportional control

---
+++ src/main/java/robot/subsystems/Commands/ProportionalVisionDriveCommand.java
@@ -0,0 +1,62 @@
+package robot.subsystems.Commands;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.command.Command;
+import robot.Robot;
+import robot.subsystems.DrivetrainConstants;
+
+public class ProportionalVisionDriveCommand extends Command {
+
+    private NetworkTableEntry distanceEntry = Robot.visionTable.getEntry("distance");
+    private NetworkTableEntry angleEntry = Robot.visionTable.getEntry("angle");
+    private double targetDistance;
+    private double distanceError;
+    private double angleError;
+    private double distanceOutput;
+    private double angleOutput;
+
+
+    public ProportionalVisionDriveCommand(double targetDistance) {
+
+        requires(Robot.drivetrain);
+        this.targetDistance = targetDistance;
+
+    }
+
+    @Override
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    @Override
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        distanceError = targetDistance - distanceEntry.getDouble(0);
+        angleError = angleEntry.getDouble(0);
+
+        distanceOutput = distanceError * DrivetrainConstants.visionDistanceKp;
+        angleOutput = angleError * DrivetrainConstants.visionAngleKp;
+
+        Robot.drivetrain.setRightSpeed(distanceOutput + angleOutput);
+        Robot.drivetrain.setLeftSpeed(distanceOutput - angleOutput);
+
+    }
+
+    @Override
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Math.abs(angleError) < 4 && Math.abs(distanceError) < 0.3;
+    }
+
+    @Override
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    @Override
+    // Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
